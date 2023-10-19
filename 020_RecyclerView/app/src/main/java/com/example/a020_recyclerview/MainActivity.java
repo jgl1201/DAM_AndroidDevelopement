4b7a17369ed0d89629deab7e087ee6dad3b59c58
jgl1201 <jorgegarciacebem@gmail.com>
2023-10-19 12:17:52 +0200

    Showing country's list

--- 020_RecyclerView/app/src/main/java/com/example/a020_recyclerview/MainActivity.java
+++ 020_RecyclerView/app/src/main/java/com/example/a020_recyclerview/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.a020_recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // very poor data management
+        List<String> list = new ArrayList<>();
+        list.add("China");
+        list.add("France");
+        list.add("Germany");
+        list.add("India");
+        list.add("Russia");
+        list.add("UK");
+        list.add("US");
+
+        // data management end
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        // to charge the list on each elemento.xml
+        recyclerView.setLayoutManager(linearLayoutManager);
+        MyAdapter myAdapter = new MyAdapter(list);
+        recyclerView.setAdapter(myAdapter);
     }
 }
