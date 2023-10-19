30e43dd7d5e7256e083f9eb14cb08f0011d9ebb5
jgl1201 <jorgegarciacebem@gmail.com>
2023-10-19 11:39:11 +0200

    MyAdapter Class

---
+++ 020_RecyclerView/app/src/main/java/com/example/a020_recyclerview/MyAdapter.java
@@ -0,0 +1,45 @@
+package com.example.a020_recyclerview;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    private List<String> nameList;
+    public MyAdapter(List<String> list) {
+        nameList = list;
+    }
+    
+    @NonNull
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    // clase INTERNA para manipular elemento.xml
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.texto);
+        }
+    }
+}
