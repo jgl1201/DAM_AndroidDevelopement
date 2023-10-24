2de4791542afc6a140df1a01a10202fdee00b756
jgl1201 <jorgegarciacebem@gmail.com>
2023-10-24 11:51:27 +0200

    Landscape layout

--- 021_ImgRecyclerView/app/build.gradle.kts
+++ 021_ImgRecyclerView/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.a021_imgrecyclerview"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.a021_imgrecyclerview"
